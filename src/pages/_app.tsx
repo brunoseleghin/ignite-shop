94ff19592e33ffd8ac4830bd55c53b54c1d99938
brunoseleghin <brunoseleghin@gmail.com>
2024-04-07 21:30:41 -0300

    changed app to added globalStyles and Logo in Header

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from "next/app";
+import Image from "next/image";
+
+import logo from '../assets/logo.svg'
+
+import { globalStyles } from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
+}
