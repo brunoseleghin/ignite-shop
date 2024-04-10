b65630abe934bd5451b12e780ac6fdd836644980
brunoseleghin <brunoseleghin@gmail.com>
2024-04-09 22:20:05 -0300

    changed getServerSide to getStaticProps to consume products from stripe API every 2 hours and format price inside from getStaticProps]

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 import Stripe from "stripe";
 import { useKeenSlider } from 'keen-slider/react'
@@ -32,12 +32,7 @@ export default function Home({ products }: HomeProps) {
           <Image src={product.imageUrl} width={570} height={480} alt="" />
           <footer>
             <strong>{product.name}</strong>
-            <span>
-              {product.price.toLocaleString('pt-BR', {
-                style: 'currency',
-                currency: 'BRL'        
-              })}
-            </span>
+            <span>{product.price}</span>
           </footer>
         </Product>
         )
@@ -47,12 +42,10 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async ({  }) => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
-
-
 
   const products = response.data.map(product => {
     const price = product.default_price as Stripe.Price
@@ -61,13 +54,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100)
     }
   })
   
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours
   }
 }
