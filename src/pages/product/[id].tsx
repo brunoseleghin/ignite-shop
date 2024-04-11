47c19855b2257b875906cf3747e66884a945bea8
brunoseleghin <brunoseleghin@gmail.com>
2024-04-10 22:09:48 -0300

    changed product page to add the structure of the page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,24 @@
 import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
+import Image from 'next/image'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis voluptatem iste laboriosam similique vitae ullam aliquam quam fuga praesentium doloremque dolore, nihil voluptates quaerat consequatur ad ex velit fugit non!</p>
+      
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
