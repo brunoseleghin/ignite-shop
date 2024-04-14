708134b7ae5f06fb64a2e8d66c1f891859cba6e8
brunoseleghin <brunoseleghin@gmail.com>
2024-04-14 12:15:44 -0300

    added validation to verify if sessionId is valid

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -31,7 +31,16 @@ export default function Success({ customerName, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const sessionId = String(query.session_id);
+  const sessionId = query.session_id && String(query.session_id);
+
+  if (!sessionId) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
     expand: ['line_items', 'line_items.data.price.product']
