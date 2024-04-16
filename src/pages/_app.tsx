85839a8510dda84b46a7a86e1cfadcad23f6b543
brunoseleghin <brunoseleghin@gmail.com>
2024-04-15 23:45:34 -0300

    added icon Handbag in Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,25 @@
 import { AppProps } from "next/app";
 import Image from "next/image";
 
+import { Handbag } from "@phosphor-icons/react";
+
 import logo from '../assets/logo.svg'
 
 import { globalStyles } from "../styles/global";
-import { Container, Header } from "../styles/pages/app";
+import { Container, Header, IconContainer } from "../styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+
   return (
     <Container>
       <Header>
         <Image src={logo} alt="" />
+
+        <IconContainer>
+          <Handbag color="#C4C4CC" size={24} weight="bold" />
+        </IconContainer>
       </Header>
 
       <Component {...pageProps} />
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -5,12 +5,28 @@ export const Container = styled('div', {
   flexDirection: 'column',
   alignItems: 'flex-start',
   justifyContent: 'center',
-  minHeight: '100vh'
+  minHeight: '100vh',
+  padding: '0 0 0 2rem'
 })
 
 export const Header = styled('header', {
+  display: 'flex',
+  justifyContent: 'space-between',
   padding: '2rem 0',
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto'
+})
+
+export const IconContainer = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  textAlign: 'center',
+
+  padding: '12px',
+  backgroundColor: '$gray800',
+  border: 0,
+  borderRadius: 6,
+  
+  cursor: 'pointer',
 })
